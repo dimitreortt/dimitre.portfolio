4d20c198fc2a176c1a63a2f52f52931b8a4df263
Dimitre Ortt <dimiortt@gmail.com>
2022-08-15 18:04:03 -0300

    Started ProjectTechItem

---
+++ src/view/Components/ProjectList/ProjectTechItem.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from "react";
+
+type Props = { tech: Tech };
+
+export const ProjectTechItem: FunctionComponent<Props> = ({}) => {
+  return <div>ProjectTechItem</div>;
+};
